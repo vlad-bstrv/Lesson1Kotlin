d4ef7aa0e767e552d800d62048f7d0de24378f3f
vlad-bstrv <keocera@icloud.com>
2022-01-09 19:49:46 +0300

    finish hw

--- app/src/main/java/com/vladbstrv/lesson1/MainActivity.kt
+++ app/src/main/java/com/vladbstrv/lesson1/MainActivity.kt
@@ -1,14 +1,45 @@
-package com.vladbstrv.lesson1;
+package com.vladbstrv.lesson1
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.vladbstrv.lesson1.R
 
-import android.os.Bundle;
+class MainActivity : AppCompatActivity() {
 
-public class MainActivity extends AppCompatActivity {
+    lateinit var button: Button
+    lateinit var textView: TextView
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.tv_test)
+        button = findViewById(R.id.btn_test)
+        button.setOnClickListener {
+            val weather = Weather()
+            textView.text = weather.town
+        }
+
+        findViewById<Button>(R.id.btn_object_text).setOnClickListener {
+            textView.text = Repo.getText()
+        }
+
+        cycleTest()
+    }
+
+    fun cycleTest() {
+        val list = arrayListOf("a", "b", "c", "d")
+
+        for(i in list) {
+            println(i)
+        }
+
+        for(i in 1..10 step 2) {
+            println("hello " + i)
+        }
     }
 }
---
+++ app/src/main/java/com/vladbstrv/lesson1/Repo.kt
@@ -0,0 +1,9 @@
+package com.vladbstrv.lesson1
+
+object Repo {
+    private val text = "text"
+
+    fun getText(): String {
+        return text
+    }
+}
